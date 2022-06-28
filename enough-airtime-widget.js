4c210c22f160ff6f0f765d0e942aabe57389a7d2
Boitumelo Reetsang <BReetsang12@gmail.com>
2022-06-28 17:37:07 +0200

    commit the first two widgets

---
+++ enough-airtime-widget.js
@@ -0,0 +1,18 @@
+
+document.addEventListener('alpine:init', () => {
+
+    Alpine.data('enoughAirtimeWidget', function () {
+        return {
+            airtimeUsage:'',
+            availableAmount:0.00,
+            airtimeMessage:'',
+            calculate() {
+
+                const amount= enoughAirtime(this.airtimeUsage,this.availableAmount);
+                this.airtimeMessage=amount;
+            }
+        }
+
+    });
+
+})
---
+++ greet.js
@@ -0,0 +1,10 @@
+function greet(name){
+  var result = "Hello, " +  name;
+  //if blank name, request the user to enter name
+  if (!name) {
+    return "Please enter your name!";
+  }
+  return result;
+    console.log(result);
+  }
+  
